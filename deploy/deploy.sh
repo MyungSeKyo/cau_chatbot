c5a0a6ba3a25538221d2ed8ed372ca740cb640ca
oliver <myungsekyo@gmail.com>
2018-08-06 22:37:06 +0900

    update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,7 @@ export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
 pip3 install virtualenv;
 virtualenv -p python3 $HOME/cau_chatbot/env;
-source $HOME/cau_chatbot/env/bin/activate;
+./$HOME/cau_chatbot/env/bin/activate;
 pip install -r $HOME/cau_chatbot/requirements.txt;
 rm -rf /etc/nginx/sites-available/default;
 ln -s $HOME/cau_chatbot/deploy/nginx.conf /etc/nginx/sites-available/default;
